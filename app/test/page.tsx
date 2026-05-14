c8a892dda7cd3536c830bbc1e26878b63dc2cf6b
ajarek <ajarek@poczta.onet.pl>
2026-05-14 21:58:55 +0200

    feat: add neon database serverless dependency and implement comment submission page

---
+++ app/test/page.tsx
@@ -0,0 +1,20 @@
+// File: app/page.tsx
+import { neon } from '@neondatabase/serverless';
+
+export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment') as string;
+    // Insert the comment from the form into the Postgres database
+    await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+  }
+
+  return (
+    <form action={create} className='min-h-screen bg-linear-to-b from-primary/20 to-secondary/10 flex flex-col items-center justify-start gap-8 px-8 sm:px-16 lg:px-24 pt-32'>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
