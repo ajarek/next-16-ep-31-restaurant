0e543fb2097abae83b4d4605463521eed8dd5e56
ajarek <ajarek@poczta.onet.pl>
2026-05-14 22:54:12 +0200

    feat: install better-auth and initialize client configuration

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
-    baseURL: "https://ep-ancient-wildflower-aje8bn46.neonauth.c-3.us-east-2.aws.neon.tech/neondb",
+    baseURL: "https://ep-ancient-wildflower-aje8bn46.neonauth.c-3.us-east-2.aws.neon.tech/neondb/auth",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
